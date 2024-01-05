72c26d34892cf6bfa906afeb019d6003781c44cf
Mario Bălănică <mariobalanica02@gmail.com>
2024-01-05 19:30:54 +0200

    Platform/RPi5: Add FDT system table support
    Signed-off-by: Mario Bălănică <mariobalanica02@gmail.com>

--- Platform/RaspberryPi/Drivers/FdtDxe/FdtDxe.c
+++ Platform/RaspberryPi/Drivers/FdtDxe/FdtDxe.c
@@ -10,9 +10,12 @@
 
 #include <Library/BaseLib.h>
 #include <Library/BaseMemoryLib.h>
+#include <Library/BoardInfoLib.h>
+#include <Library/BoardRevisionHelperLib.h>
 #include <Library/DebugLib.h>
 #include <Library/DxeServicesLib.h>
 #include <Library/FdtLib.h>
+#include <Library/FdtPlatformLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
@@ -23,6 +26,7 @@
 STATIC VOID                             *mFdtImage;
 
 STATIC RASPBERRY_PI_FIRMWARE_PROTOCOL   *mFwProtocol;
+STATIC UINT32                           mBoardRevision;
 
 STATIC
 EFI_STATUS
@@ -462,9 +466,15 @@ FdtDxeInitialize (
                   (VOID**)&mFwProtocol);
   ASSERT_EFI_ERROR (Status);
 
-  FdtImage = (VOID*)(UINTN)PcdGet32 (PcdFdtBaseAddress);
-  Retval = FdtCheckHeader (FdtImage);
-  if (Retval != 0) {
+  Status = BoardInfoGetRevisionCode (&mBoardRevision);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR,
+        "%a: Failed to get board revision code. Status=%r\n",
+        __func__, Status));
+  }
+
+  FdtImage = FdtPlatformGetBase ();
+  if (FdtImage == NULL) {
     /*
      * Any one of:
      * - Invalid config.txt device_tree_address (not PcdFdtBaseAddress)
@@ -498,39 +508,41 @@ FdtDxeInitialize (
    * These are all best-effort.
    */
 
-  Status = SanitizePSCI ();
-  if (EFI_ERROR (Status)) {
-    Print (L"Failed to sanitize PSCI: %r\n", Status);
-  }
-
   Status = CleanMemoryNodes ();
   if (EFI_ERROR (Status)) {
-    Print (L"Failed to clean memory nodes: %r\n", Status);
+    DEBUG ((DEBUG_ERROR, "Failed to clean memory nodes: %r\n", Status));
   }
 
   Status = CleanSimpleFramebuffer ();
   if (EFI_ERROR (Status)) {
-    Print (L"Failed to clean frame buffer: %r\n", Status);
+    DEBUG ((DEBUG_ERROR, "Failed to clean frame buffer: %r\n", Status));
   }
 
-  Status = FixEthernetAliases ();
-  if (EFI_ERROR (Status)) {
-    Print (L"Failed to fix ethernet aliases: %r\n", Status);
-  }
+  if (BoardRevisionGetModelFamily (mBoardRevision) < 5) {
+    Status = SanitizePSCI ();
+    if (EFI_ERROR (Status)) {
+      DEBUG ((DEBUG_ERROR, "Failed to sanitize PSCI: %r\n", Status));
+    }
 
-  Status = UpdateMacAddress ();
-  if (EFI_ERROR (Status)) {
-    Print (L"Failed to update MAC address: %r\n", Status);
-  }
+    Status = FixEthernetAliases ();
+    if (EFI_ERROR (Status)) {
+      DEBUG ((DEBUG_ERROR, "Failed to fix ethernet aliases: %r\n", Status));
+    }
 
-  Status = AddUsbCompatibleProperty ();
-  if (EFI_ERROR (Status)) {
-    Print (L"Failed to update USB compatible properties: %r\n", Status);
-  }
+    Status = UpdateMacAddress ();
+    if (EFI_ERROR (Status)) {
+      DEBUG ((DEBUG_ERROR, "Failed to update MAC address: %r\n", Status));
+    }
 
-  SyncPcie ();
-  if (EFI_ERROR (Status)) {
-    Print (L"Failed to update PCIe address ranges: %r\n", Status);
+    Status = AddUsbCompatibleProperty ();
+    if (EFI_ERROR (Status)) {
+      DEBUG ((DEBUG_ERROR, "Failed to update USB compatible properties: %r\n", Status));
+    }
+
+    Status = SyncPcie ();
+    if (EFI_ERROR (Status)) {
+      DEBUG ((DEBUG_ERROR, "Failed to update PCIe address ranges: %r\n", Status));
+    }
   }
 
   DEBUG ((DEBUG_INFO, "Installed devicetree at address %p\n", mFdtImage));
